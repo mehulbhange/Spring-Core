c9dfabbff480870b515bc6a69d105923c8036f5b
mehulbhange <bhangemehul@gmail.com>
2022-10-07 18:40:00 +0530

    ADDED:[Mehul] logger to log messages

--- src/main/java/com/bridgelabz/springcoreconcept/SpringCoreConceptApplication.java
+++ src/main/java/com/bridgelabz/springcoreconcept/SpringCoreConceptApplication.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.springcoreconcept;
 
 import com.bridgelabz.springcoreconcept.components.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,12 +10,14 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class SpringCoreConceptApplication {
 
+    public static final Logger logger = LoggerFactory.getLogger(SpringCoreConceptApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Spring Concepts Demo");
+        logger.info("Welcome to Spring Concepts Demo");
         ApplicationContext context = SpringApplication.run(SpringCoreConceptApplication.class, args);
         DemoBean demoBean = context.getBean(DemoBean.class);
 
-        System.out.println("DemoBean = "+ demoBean.toString());
+        logger.debug("DemoBean = "+ demoBean.toString());
     }
 
 }
